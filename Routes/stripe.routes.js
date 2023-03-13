5d842b4cd3faa920c1eff98ab3e3923c7d6807d5
Dhanush Saji <dhanushsaji1@gmail.com>
2023-03-13 12:29:18 +0530

    deploy4

--- Routes/stripe.routes.js
+++ Routes/stripe.routes.js
@@ -8,68 +8,74 @@ stripeRouter.post('/test',async(req,res)=>{
 })
 let tempData;
 stripeRouter.post('/create-checkout-session', async (req, res) => {
-  const customer = await stripe.customers.create({
-    metadata:{
-      userId:req.body.userId,
-    }
-  })
-  tempData=req.body.cartItems
-  const line_items=req.body.cartItems.map((item)=>{
-    return{
-      price_data: {
-        currency: 'inr',
-        product_data: {
-          name: item.title,
-          images:[item.imageurl],
-          description:item.des,
-          metadata:{
-            id:item._id
-          }
+  try {
+    const customer = await stripe.customers.create({
+      metadata:{
+        userId:req.body.userId,
+      }
+    })
+    tempData=req.body.cartItems
+    const line_items=req.body.cartItems.map((item)=>{
+      return{
+        price_data: {
+          currency: 'inr',
+          product_data: {
+            name: item.title,
+            images:[item.imageurl],
+            description:item.des,
+            metadata:{
+              id:item._id
+            }
+          },
+          unit_amount: item.price*100,
         },
-        unit_amount: item.price*100,
-      },
-      quantity: item.quantity,
-    }
-  })
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types:['card'],
-      shipping_address_collection: {allowed_countries: ['IN']},
-      shipping_options: [
-        {
-          shipping_rate_data: {
-            type: 'fixed_amount',
-            fixed_amount: {amount: 0, currency: 'inr'},
-            display_name: 'Free shipping',
-            delivery_estimate: {
-              minimum: {unit: 'business_day', value: 5},
-              maximum: {unit: 'business_day', value: 7},
+        quantity: item.quantity,
+      }
+    })
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types:['card'],
+        shipping_address_collection: {allowed_countries: ['IN']},
+        shipping_options: [
+          {
+            shipping_rate_data: {
+              type: 'fixed_amount',
+              fixed_amount: {amount: 0, currency: 'inr'},
+              display_name: 'Free shipping',
+              delivery_estimate: {
+                minimum: {unit: 'business_day', value: 5},
+                maximum: {unit: 'business_day', value: 7},
+              },
             },
           },
-        },
-        {
-          shipping_rate_data: {
-            type: 'fixed_amount',
-            fixed_amount: {amount: 8000, currency: 'inr'},
-            display_name: 'Fast shipping',
-            delivery_estimate: {
-              minimum: {unit: 'business_day', value: 1},
-              maximum: {unit: 'business_day', value: 2},
+          {
+            shipping_rate_data: {
+              type: 'fixed_amount',
+              fixed_amount: {amount: 8000, currency: 'inr'},
+              display_name: 'Fast shipping',
+              delivery_estimate: {
+                minimum: {unit: 'business_day', value: 1},
+                maximum: {unit: 'business_day', value: 2},
+              },
             },
           },
+        ],
+        phone_number_collection:{
+          enabled:true
         },
-      ],
-      phone_number_collection:{
-        enabled:true
-      },
-      line_items,
-      customer:customer.id,
-      mode: 'payment',
-      success_url: `${process.env.CLIENT_URL}/checkout-success`,
-      cancel_url: `${process.env.CLIENT_URL}/error`,
-    });
-  
-    res.send(JSON.stringify({url:session.url}));
+        line_items,
+        customer:customer.id,
+        mode: 'payment',
+        success_url: `${process.env.CLIENT_URL}/checkout-success`,
+        cancel_url: `${process.env.CLIENT_URL}/error`,
+      });
+    
+      res.send(JSON.stringify({url:session.url}));
+  } catch (error) {
+    res.status(400).json({error:'error in session creation'})
+  }
+ 
   });
+
   const createOrder = async (customer, data) => {
     var items = tempData
     
@@ -126,13 +132,14 @@ stripeRouter.post('/webhook', express.raw({type: 'application/json'}), async(req
   let payment
   // Handle the event
   if (eventType === "checkout.session.completed") {
-    console.log('hi checkout session working')
+    res.send('haii')
     stripe.customers
       .retrieve(data.customer)
       .then(async (customer) => {
         try {
           // CREATE ORDER
-          createOrder(customer, data);
+          res.send('haii')
+          // createOrder(customer, data);
         } catch (err) {
           console.log(typeof createOrder);
           console.log(err);
